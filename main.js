4ed6b781a55fa686cb14aa75f6b1d3a882a0d003
Rexy☆ <149522903+irxy@users.noreply.github.com>
2024-01-31 13:43:13 +0100

    Add files via upload
    lexy bot

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('myButton').addEventListener('click', function() {
+  document.getElementById('myPopup').style.display = 'block';
+});
+
+function closePopup() {
+  document.getElementById('myPopup').style.display = 'none';
+}
